31c6a84f1dee01f9c6af5ae46315ed5b2aab53fb
nicholas <nicholas_ngoh_from.scientec@stb.gov.sg>
2024-07-24 16:49:33 +0800

    Add main page that combines all the components together. Call list of api and submit api

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Shortener from "./urlListing/shortner";
+import './layout';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function Urls() {
+  return <Shortener />
+}
